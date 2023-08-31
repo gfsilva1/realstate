580b06bc85313109e8b926ac3c288871b29193f6
Guilherme Teixeira <guilherme.t.silva@bioritmo.com.br>
2023-08-31 10:36:51 -0300

    yarn

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GreenBasket
+module RealState
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
